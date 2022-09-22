127c655646e5ce797705a98e213d2d38cb56c989
giangcse <giangb1609816@student.ctu.edu.vn>
2022-09-22 08:43:28 +0700

    Commit lần đầu

---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+import json
+import pandas as pd
+import uvicorn
+import pymongo
+import validators
+
+from bs4 import BeautifulSoup
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi import responses
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+myclient = pymongo.MongoClient('mongodb://localhost:27017')
+mydb = myclient['tailieuhoctap']
+mycol = mydb['instagram']
+
+headersList = {
+ "Accept": "*/*",
+ "User-Agent": "Thunder Client (https://www.thunderclient.com)" 
+}
+payload = ""
+
+class Data(BaseModel):
+    url:str
+
+def getInfoUser_instagram(url_profile):
+    try:
+        response = requests.request("GET", url_profile, data=payload,  headers=headersList)
+
+        data = BeautifulSoup(response.text, "html")
+        info = str(data.title).split('•')[0][7:]
+
+        idx = (str(data).index('"profile_pic_url":'))
+        url_pic = (str(data)[idx+19:idx+500].split('"')[0].replace('\\', ''))
+
+        mycol.insert_one({"user_name": str(info), "profile_picture": str(url_pic), "url_profile": url_profile})
+    except Exception as e:
+        return e
+
+def readFile(file_path):
+    data = pd.read_csv(file_path)
+    return data.iloc[:,0]
+
+@app.get('/')
+async def root():
+    data = []
+    for d in mycol.find({},{'_id':0}):
+        data.append(d)
+
+    return (json.loads(json.dumps(data)))
+
+@app.get('/add')
+async def add(url: str = Form(...)):
+    return {"url": url}
+
+def saveToDB():
+    try:
+        urls = readFile('E:\\Cong chien\\Linh tinh\\insta\\url.csv')
+        data = []
+        for url in urls:
+            data.append(getInfoUser_instagram(url))
+        return data
+    except Exception as e:
+        return e
+
+if __name__=='__main__':
+    # saveToDB()
+    uvicorn.run("main:app", host="0.0.0.0", port=81, debug=True)
